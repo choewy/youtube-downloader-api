efde322b951be94c7c14dabe700f60a88b9cf0db
최원영 <choewy32@gmail.com>
2026-07-24 08:02:23 +0900

    chore: settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,15 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       globals: {
@@ -25,11 +26,32 @@ export default tseslint.config(
     },
   },
   {
+    plugins: {
+      'simple-import': simpleImportSort,
+    },
+  },
+  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'simple-import/imports': [
+        'error',
+        {
+          groups: [['^\\u0000'], ['^@nestjs(/.*|$)'], ['^node:', '^(?!@nestjs)(@?\\w)'], ['^@libs(/.*|$)'], ['^@apps(/.*|$)'], ['^\\.\\./'], ['^\\./']],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.spec.ts', '**/*.test.ts', '**/test/**/*.ts', '**/__tests__/**/*.ts'],
+    rules: {
+      '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
     },
   },
 );
